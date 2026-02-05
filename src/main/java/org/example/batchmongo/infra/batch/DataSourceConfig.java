628add5032029b16f8944cfb839ee2989e8e0ef7
pauloalarico <paulocraft20@gmail.com>
2026-02-04 23:58:24 -0300

    refactor - add the bean for the spring

--- src/main/java/org/example/batchmongo/infra/batch/DataSourceConfig.java
+++ src/main/java/org/example/batchmongo/infra/batch/DataSourceConfig.java
@@ -3,6 +3,7 @@ package org.example.batchmongo.infra.batch;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
@@ -14,6 +15,7 @@ public class DataSourceConfig {
     private String password;
     private String url;
 
+    @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(url);
