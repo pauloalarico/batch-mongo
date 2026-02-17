956dd19931c33230b0560a81219296660204990d
pauloalarico <paulocraft20@gmail.com>
2026-02-16 23:46:00 -0300

    refactor - add constructor for the dto receive the entity.

--- src/main/java/org/example/batchmongo/application/RedisDto.java
+++ src/main/java/org/example/batchmongo/application/RedisDto.java
@@ -1,9 +1,14 @@
 package org.example.batchmongo.application;
 
+import org.example.batchmongo.domain.entitie.Bank;
+
 public record RedisDto (
         String name,
         String documentNumber,
         String branch,
         String account
 ) {
+    public RedisDto(Bank bank) {
+        this(bank.getName(), bank.getDocumentNumber(), bank.getBranch(), bank.getAccount());
+    }
 }
