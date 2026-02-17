f3f1f129ebb590eee8a21a8fa4118905c8b6cc17
pauloalarico <paulocraft20@gmail.com>
2026-02-16 23:55:34 -0300

    refactor - change the serialization of the redis for the convention.

--- src/main/java/org/example/batchmongo/infra/batch/RedisConfiguration.java
+++ src/main/java/org/example/batchmongo/infra/batch/RedisConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
@@ -13,9 +13,8 @@ public class RedisConfiguration {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.json());
         template.afterPropertiesSet();
         return template;
     }
